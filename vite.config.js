e30012e427cf86bcc0d745dea5dc9076ff574774
CerereBaccho <127483765+CerereBaccho@users.noreply.github.com>
2025-09-13 21:33:32 +0900

    vite.config.js を更新

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
+// vite.config.ts
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/stocklite/',   // GitHub Pages のサブパス
-  build: {
-    outDir: 'dist',      // 直接 docs に出力
-    emptyOutDir: false,  // 既存の docs を全部消したくない場合は false（そのまま）
-  },
-});
+  base: '/stocklite/',   // ← リポジトリ名
+  // build: { outDir: 'dist' } // 明示しなくてもOK（デフォルトが dist）
+})
